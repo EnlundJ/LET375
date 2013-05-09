fe993d20f51ea9369baeea1e1b46fd9889cf4678
Niklas Beischer <niklas.beischer@gmail.com>
2013-05-09 10:46:22 +0200

    Create graph while knocking down walls in maze and implement Maze::search()

--- lab6_maze/src/Maze.java
+++ lab6_maze/src/Maze.java
@@ -1,12 +1,15 @@
+import java.util.List;
 
 public class Maze extends Board {
 	
 	private DisjointSets set;
+	private ExtendedGraph graph;
   		
     public Maze( int rows, int cols )
 	{
     	super(rows,cols);
     	set = new DisjointSets(rows*cols);
+    	graph = new ExtendedGraph();
     }
     
     public void create()
@@ -49,22 +52,44 @@ public class Maze extends Board {
     			second.move(direction); // Move back
     			second.move(direction); // Move in opposite direction
     		}
+    		int firstId = getCellId(first);
+    		int secondId = getCellId(second);
     		
     		// Knock down the wall if the second cell is not a part of the same set
-    		int firstRoot = set.find(getCellId(first));
-    		int secondRoot = set.find(getCellId(second));
+    		int firstRoot = set.find(firstId);
+    		int secondRoot = set.find(secondId);
     		if(firstRoot != secondRoot)
     		{
-    			setChanged();
+    			set.union(firstRoot, secondRoot);	// Join the sets
+
+    			// Knock down the wall 
+    			setChanged();							
     			notifyObservers(new Pair<Point, Point>(first, second));
-    			set.union(firstRoot, secondRoot);
     			knockedWalls++;
+    			
+    			// Add edges in graph between the cells
+    			graph.addEdge(firstId, secondId, 1);
+    			graph.addEdge(secondId, firstId, 1);
     		}
     	}
-    }
+   }
     
-    public void search() {
-//    	 Implement this method!
+    public void search()
+    {
+    	graph.unweighted(0);
+//    	graph.printPath(maxCell-1);
+    	List<Integer> list = graph.getPath(maxCell-1);
+    	
+    	for(Integer i : list)
+    	{
+    		setChanged();
+    		notifyObservers(new Point(getRow(i), getCol(i)));
+    	}
+/*    	for(Integer i : list)
+    	{
+    		System.out.println(" "+ getCol(i) + ":" + getRow(i));
+    	}
+*/
     }
     
 //    ...
