6be6dcd1b70b551ad1519fec1ad97b0e77238fa6
Niklas Beischer <niklas.beischer@gmail.com>
2013-05-09 10:43:11 +0200

    Added implementation of ExtendedGraph

---
+++ lab6_maze/src/ExtendedGraph.java
@@ -0,0 +1,31 @@
+import java.util.Iterator;
+import java.util.Collection;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.Map;
+import java.util.LinkedList;
+
+
+public class ExtendedGraph extends Graph
+{
+	public List<Integer> getPath(int destName)
+	{
+		return getPath(vertexMap.get(destName));
+	}
+	
+	private List<Integer> getPath(Vertex dest)
+	{
+        if( dest.prev != null )
+        {
+        	List<Integer> list = getPath(dest.prev);
+        	list.add(dest.name);
+            return list;
+        }
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(dest.name);
+        return list;
+	}
+
+}
